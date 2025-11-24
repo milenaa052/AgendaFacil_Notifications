754f8641d13a8a73109181851db5ddb855fa7123
milenaa052 <milenasantosdeoliveira40@gmail.com>
2025-11-24 01:51:42 -0300

    feat: linking scheduling to notifications

--- notifications-service/src/http/http.service.ts
+++ notifications-service/src/http/http.service.ts
@@ -5,16 +5,27 @@ import axios, { AxiosInstance } from 'axios';
 @Injectable()
 export class HttpService {
 
-    private client: AxiosInstance;
+    private usersClient: AxiosInstance;
+    private schedulingClient: AxiosInstance;
 
     constructor(cfg: ConfigService) {
-        this.client = axios.create({
+
+        this.usersClient = axios.create({
             baseURL: cfg.get('USERS_API', 'http://service-users:3000'),
+            timeout: 5000,
+        });
+
+        this.schedulingClient = axios.create({
+            baseURL: cfg.get('SCHEDULING_API', 'http://service-scheduling:3000'),
             timeout: 5000,
         });
     }
 
-    get instance() {
-        return this.client;
+    get users() {
+        return this.usersClient;
+    }
+
+    get scheduling() {
+        return this.schedulingClient;
     }
 }
